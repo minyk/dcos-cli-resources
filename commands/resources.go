3470edfe13f76054f2d671b844fbf3d1e2e1a1cd
minyk <minykreva@gmail.com>
2019-08-19 14:02:51 +0900

    Add destroy persistence volume. Add unreserve disk resource.

--- commands/resources.go
+++ commands/resources.go
@@ -6,14 +6,19 @@ import (
 )
 
 type resourcesHandler struct {
-	q         *queries.Resources
-	agentID   string
-	role      string
-	principal string
-	cpus      float64
-	cpuLabel  string
-	mem       float64
-	memLabel  string
+	q             *queries.Resources
+	agentID       string
+	role          string
+	principal     string
+	cpus          float64
+	cpuLabel      string
+	mem           float64
+	memLabel      string
+	disk          float64
+	diskLabel     string
+	persistid     string
+	containerpath string
+	hostpath      string
 }
 
 func (cmd *resourcesHandler) handleReserve(a *kingpin.Application, e *kingpin.ParseElement, c *kingpin.ParseContext) error {
@@ -21,13 +26,23 @@ func (cmd *resourcesHandler) handleReserve(a *kingpin.Application, e *kingpin.Pa
 }
 
 func (cmd *resourcesHandler) handleUnreserve(a *kingpin.Application, e *kingpin.ParseElement, c *kingpin.ParseContext) error {
-	return cmd.q.UnreserveResource(cmd.agentID, cmd.role, cmd.principal, cmd.cpus, cmd.cpuLabel, cmd.mem, cmd.memLabel)
+	return cmd.q.UnreserveResource(cmd.agentID, cmd.role, cmd.principal, cmd.cpus, cmd.cpuLabel, cmd.mem, cmd.memLabel, cmd.disk, cmd.diskLabel)
+}
+
+func (cmd *resourcesHandler) handleUnreserveAll(a *kingpin.Application, e *kingpin.ParseElement, c *kingpin.ParseContext) error {
+	return cmd.q.UnreserveResourceAll(cmd.agentID, cmd.role, cmd.principal)
+}
+
+func (cmd *resourcesHandler) handleDestroyPersistVolume(a *kingpin.Application, e *kingpin.ParseElement, c *kingpin.ParseContext) error {
+	return cmd.q.DestroyVolume(cmd.agentID, cmd.role, cmd.principal, cmd.disk, cmd.diskLabel, cmd.persistid, cmd.containerpath, cmd.hostpath)
 }
 
 // HandleScheduleSection
 func HandleResourcesSection(app *kingpin.Application, q *queries.Resources) {
 	HandleReserveResourcesCommands(app.Command("reserve", "Reserve resources").Alias("reserves"), q)
 	HandleUnreserveResourcesCommands(app.Command("unreserve", "Unreserve resources").Alias("unreserves"), q)
+	HandleUnreserveResourcesAllCommands(app.Command("unreserve-all", "Unreserve all resources").Alias("unreservesall"), q)
+	HandleDestroyPersistVolume(app.Command("destroy-persist-volume", "Destroy persistence volume").Alias("destroyvolume"), q)
 }
 
 // HandleScheduleCommand
@@ -52,4 +67,28 @@ func HandleUnreserveResourcesCommands(resources *kingpin.CmdClause, q *queries.R
 	unReserve.Flag("cpus-resource-id", "Resource id for unreserve action.").Default("").StringVar(&cmd.cpuLabel)
 	unReserve.Flag("mem", "Amount of memory to unreserve. The unit is MB.").Default("0").Float64Var(&cmd.mem)
 	unReserve.Flag("mem-resource-id", "Resource id for unreserve action.").Default("").StringVar(&cmd.memLabel)
+	unReserve.Flag("disk", "Amount of disk to unreserve").Default("0").Float64Var(&cmd.disk)
+	unReserve.Flag("disk-resource-id", "Resource id for unreserve action.").Default("").StringVar(&cmd.diskLabel)
+}
+
+// Unreserve all resources with role and principal
+func HandleUnreserveResourcesAllCommands(resources *kingpin.CmdClause, q *queries.Resources) {
+	cmd := &resourcesHandler{q: q}
+	unReserve := resources.Action(cmd.handleUnreserve)
+	unReserve.Flag("agent-id", "Agent ID to unreserve").Required().StringVar(&cmd.agentID)
+	unReserve.Flag("role", "Role for unreserve").Required().StringVar(&cmd.role)
+	unReserve.Flag("principal", "Principal for unreserve.").Default("my-principal").StringVar(&cmd.principal)
+}
+
+func HandleDestroyPersistVolume(resources *kingpin.CmdClause, q *queries.Resources) {
+	cmd := &resourcesHandler{q: q}
+	destroyPersistVolume := resources.Action(cmd.handleDestroyPersistVolume)
+	destroyPersistVolume.Flag("agent-id", "Agent ID to unreserve").Required().StringVar(&cmd.agentID)
+	destroyPersistVolume.Flag("role", "Role for unreserve").Required().StringVar(&cmd.role)
+	destroyPersistVolume.Flag("principal", "Principal for unreserve.").Default("my-principal").StringVar(&cmd.principal)
+	destroyPersistVolume.Flag("disk", "Amount of disk to unreserve").Default("0").Float64Var(&cmd.disk)
+	destroyPersistVolume.Flag("disk-resource-id", "Resource id for unreserve action.").Default("").StringVar(&cmd.diskLabel)
+	destroyPersistVolume.Flag("disk-persist-id", "Persistence id for unreserve action.").Default("").StringVar(&cmd.persistid)
+	destroyPersistVolume.Flag("container-path", "Container path of disk.").Default("").StringVar(&cmd.containerpath)
+	destroyPersistVolume.Flag("host-path", "host path of disk.").Default("").StringVar(&cmd.hostpath)
 }
--- queries/resources.go
+++ queries/resources.go
@@ -3,6 +3,7 @@ package queries
 import (
 	"encoding/json"
 	"github.com/mesos/mesos-go/api/v1/lib"
+	"github.com/mesos/mesos-go/api/v1/lib/agent"
 	"github.com/mesos/mesos-go/api/v1/lib/master"
 	"github.com/minyk/dcos-resources/client"
 )
@@ -44,7 +45,7 @@ func (q *Resources) ReserveResource(agentid string, role string, principal strin
 	return nil
 }
 
-func (q *Resources) UnreserveResource(agentid string, role string, principal string, cpus float64, cpusLabel string, mem float64, memLabel string) error {
+func (q *Resources) UnreserveResource(agentid string, role string, principal string, cpus float64, cpusLabel string, mem float64, memLabel string, disk float64, diskLabel string) error {
 
 	resources := []mesos.Resource{}
 	if cpus > 0 {
@@ -52,6 +53,9 @@ func (q *Resources) UnreserveResource(agentid string, role string, principal str
 	}
 	if mem > 0 {
 		resources = append(resources, resourceWithLabel("mem", role, principal, mem, memLabel))
+	}
+	if disk > 0 {
+		resources = append(resources, resourceWithLabel("disk", role, principal, disk, diskLabel))
 	}
 
 	body := master.Call{
@@ -63,6 +67,81 @@ func (q *Resources) UnreserveResource(agentid string, role string, principal str
 	}
 
 	requestContent, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	_, err = client.HTTPServicePostJSON("/api/v1", requestContent)
+	if err != nil {
+		return err
+	} else {
+		client.PrintMessage("Unreservation is successful.")
+	}
+
+	return nil
+
+}
+
+func (q *Resources) DestroyVolume(agentid string, role string, principal string, disk float64, resourceid string, persistid string, containerpath string, hostpath string) error {
+
+	resources := []mesos.Resource{}
+
+	resources = append(resources, resourceDiskWithLabel(role, principal, disk, resourceid, persistid, containerpath, ""))
+
+	request_body := master.Call{
+		Type: master.Call_DESTROY_VOLUMES,
+		DestroyVolumes: &master.Call_DestroyVolumes{
+			AgentID: mesos.AgentID{Value: agentid},
+			Volumes: resources,
+		},
+	}
+
+	requestContent, err := json.Marshal(request_body)
+	_, err = client.HTTPServicePostJSON("/api/v1", requestContent)
+	if err != nil {
+		return err
+	} else {
+		client.PrintMessage("Persistence Volume is successfully removed.")
+	}
+
+	return nil
+}
+
+func (q *Resources) UnreserveResourceAll(agentid string, role string, principal string) error {
+
+	// query current state from agent
+	state_body := agent.Call{
+		Type: agent.Call_GET_STATE,
+	}
+	requestContent, err := json.Marshal(state_body)
+	response, err := client.HTTPServicePostJSON("/api/v1", requestContent)
+	if err != nil {
+		return err
+	}
+
+	agentStateReponse := master.Response{}
+	json.Unmarshal(response, &agentStateReponse)
+
+	// TODO find all resources with role and principal
+	cpus := float64(4)
+	cpus_resource_id := ""
+	mem := float64(4)
+	mem_resource_id := ""
+
+	// unreserve cpus, mem resources with resource_id
+	resources := []mesos.Resource{}
+	resources = append(resources, resourceWithLabel("cpus", role, principal, cpus, cpus_resource_id))
+	resources = append(resources, resourceWithLabel("mem", role, principal, mem, mem_resource_id))
+
+	body := master.Call{
+		Type: master.Call_UNRESERVE_RESOURCES,
+		UnreserveResources: &master.Call_UnreserveResources{
+			AgentID:   mesos.AgentID{Value: agentid},
+			Resources: resources,
+		},
+	}
+
+	requestContent, err = json.Marshal(body)
 	if err != nil {
 		return err
 	}
@@ -125,5 +204,50 @@ func resourceWithLabel(resourceType string, role string, principal string, cpus 
 		Role:        &role,
 		Reservation: &reservation,
 		Scalar:      &mesos.Value_Scalar{Value: cpus},
+	}
+}
+
+func resourceDiskWithLabel(role string, principal string, disk float64, resourceid string, persistid string, containerPath string, hostPath string) mesos.Resource {
+
+	label := mesos.Label{
+		Key:   "resource_id",
+		Value: &resourceid,
+	}
+
+	labels := []mesos.Label{}
+	labels = append(labels, label)
+	mesosLabels := mesos.Labels{Labels: labels}
+
+	scala := mesos.Value_Type(mesos.SCALAR)
+	reservation := mesos.Resource_ReservationInfo{
+		Principal: &principal,
+		Labels:    &mesosLabels,
+	}
+
+	persist := mesos.Resource_DiskInfo_Persistence{
+		ID:        persistid,
+		Principal: &principal,
+	}
+
+	rw := mesos.RW
+
+	volume := mesos.Volume{
+		Mode:          &rw,
+		ContainerPath: containerPath,
+	}
+
+	diskinfo := mesos.Resource_DiskInfo{
+		Persistence: &persist,
+		Volume:      &volume,
+		Source:      nil,
+	}
+
+	return mesos.Resource{
+		Type:        &scala,
+		Name:        "disk",
+		Role:        &role,
+		Reservation: &reservation,
+		Scalar:      &mesos.Value_Scalar{Value: disk},
+		Disk:        &diskinfo,
 	}
 }
